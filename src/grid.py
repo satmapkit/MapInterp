4d313a5414caf0f33cc5f4f1d4d935c2957b1f9f
Daniel Neshyba-Rowe <rowedanielp@gmail.com>
2025-12-09 10:28:04 -0800

    Add helper function to save to netcdf

--- src/grid.py
+++ src/grid.py
@@ -2,6 +2,7 @@ from OceanDB.AlongTrack import AlongTrack
 import numpy as np
 import numpy.typing as npt
 from typing import Any
+from xarray import DataArray
 
 
 def geographic_grid_using_atdb(
@@ -19,3 +20,26 @@ def geographic_grid_using_atdb(
     lat_ocean = lat_world[ocean_indices]
     lon_ocean = lon_world[ocean_indices]
     return lat_ocean, lon_ocean
+
+
+def save_netcdf_grid(
+    x: npt.NDArray[np.floating[Any]],
+    y: npt.NDArray[np.floating[Any]],
+    sla: npt.NDArray[np.floating[Any]],
+    filename: str,
+    x_name: str = "lat",
+    y_name: str = "lon",
+    attrs: dict[str, Any] = {},
+):
+    """
+    Save sea level anomalies on a grid
+
+    x: n-array of x axis values
+    y: m-array of y axis values
+    sla: n by m-array of sea level anomalies
+    x_name: name of x-axis
+    y_name: name of y-axis
+    attrs: additional metadata
+    """
+    df = DataArray(sla, coords=[(x_name, x), (y_name, y)], name="sla", attrs=attrs)
+    df.to_netcdf(filename)
