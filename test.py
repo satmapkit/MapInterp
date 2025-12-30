dab181a10e3f0cb49bada68ea67a01799befec94
Daniel Neshyba-Rowe <rowedanielp@gmail.com>
2025-12-30 11:15:34 -0800

    Update test to import MapInterp as a module instead of directly importing files

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-from src.Interpolator import interpolate_using_atdb, NearestNeighborInterpolator, GeographicGaussianKernelInterpolator, ProjectedGaussianKernelInterpolator
-from src.Grid import InterpGrid
+from MapInterp.Interpolator import interpolate_using_atdb, NearestNeighborInterpolator, GeographicGaussianKernelInterpolator, ProjectedGaussianKernelInterpolator
+from MapInterp.Grid import InterpGrid
 
 import numpy as np
 from datetime import datetime
